e6e64e6c3e59eaa09fc89ba9ad54a120009abb08
ElioMolasDev <eliomolas.dev@gmail.com>
2023-09-19 10:54:01 -0300

    sacando navbar

--- index.js
+++ index.js
@@ -10,21 +10,6 @@ sideLinks.forEach(item =>{
   })
 });
 
-const menuBar = document.querySelector('.content nav .bx.bx-menu');
-const sideBar = document.querySelector('.sidebar');
-menuBar.addEventListener('click', () => {
-  sideBar.classList.toggle('close');
-});
-
-
-window.addEventListener('resize',() =>{
-  if(window.innerWidth < 768){
-    sideBar.classList.add('close');
-  }else{
-    sideBar.classList.remove('close');
-  }
-});
-
 const toggler = document.getElementById('theme-toggle');
 
 toggler.addEventListener('change', function () {
