8a643b2c8ee7a3a61152cdc3e77dea0a9cb4272d
ElioMolasDev <eliomolas.dev@gmail.com>
2023-09-22 23:59:20 -0300

    terminando oscuro/claro

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const toggler = document.getElementById('theme-toggle');
 
+toggler.addEventListener('change', function () {
+  if(this.checked){
+    document.body.classList.add('dark');
+  }else{
+    document.body.classList.remove('dark');
+  }
+});
 
