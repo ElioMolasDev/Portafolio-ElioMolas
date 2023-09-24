a7012bea7fcd8119644ccd198b17e5da94760dac
ElioMolasDev <eliomolas.dev@gmail.com>
2023-09-24 20:44:59 -0300

    arreglando formulario

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const nav = document.querySelector(".menu"),
     $form.addEventListener("submit", (e) => {
       e.preventDefault();
       $loader.classList.remove("none");
-      fetch("https://formsubmit.co/ajax/your@email.com", {
+      fetch("https://formsubmit.co/ajax/eliomolas.dev@gmail.com", {
         method: "POST",
         body: new FormData(e.target),
       })
